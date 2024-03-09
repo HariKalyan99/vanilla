c7a405a9443144410d432f6c3a70b8acf0ef7fa6
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-03-09 23:39:06 +0530

    dummy json data

---
+++ Todo-list/app.js
@@ -0,0 +1,78 @@
+// let productList = [];
+
+
+function fetchData() {
+    let data = fetch('https://dummyjson.com/products').then((res) => res.json()).then(({products}) => {
+        return products;
+    })
+    return data
+}
+
+
+
+
+function init() {
+    let productList = fetchData();
+    productList.then((products) => {
+       for(let i=0; i<products.length; i++){
+        displayData(products[i].price, products[i].title, products[i].images[0])
+       }
+    })
+}
+
+init()
+
+
+
+
+function displayData(price, title, images) {
+    let mainDiv = document.querySelector('.main');
+    let productContainer = document.createElement('div');
+    productContainer.setAttribute('class', "productContainer")
+    productContainer.innerHTML = `
+    <img src=${images} alt=${title} height="100px">
+    <p>${title}</p>
+    <p>${price}</p>
+    `;
+    mainDiv.append(productContainer)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addProduct() {
+//     fetch('https://dummyjson.com/products/add', {
+//         method: "POST",
+//         headers: {
+//             "Content-Type" : "application/json"
+//         },
+//         body: JSON.stringify({
+//             title: 'BMW Pencil',
+//         })
+//     }).then((res) => res.json()).then(console.log)
+// }
+
+
+
+// addProduct()
+
